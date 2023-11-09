34a76e9c1a499f72a05c87a15ce17acab0e5d973
Aminat Okunuga <makadeaminat@gmail.com>
2023-11-09 22:45:31 +0100

    ChatGPT Clone

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+import { Ai } from '@cloudflare/ai';
+import { html } from './template.js';
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		// return new Response('Hello MeenatCodes!');
+		const url = new URL(request.url);
+		if(request.method === 'POST' && url.pathname === '/ai'){
+			// Create an instance of the AI using the provided environment variable.
+			const ai = new Ai(env.AI);
+
+			const body = await request.json();
+			const userMessage = body.message;
+			const messages = [
+				{ role: 'system', content: 'You are an friendly assistant.' },
+				{ role: 'user', content: userMessage },
+			]
+
+			const response = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+				messages,
+			});
+
+			// Present the response as a JSON string.
+			return new Response(JSON.stringify(response));
+		}
+
+		return new Response(html, {
+			headers: {
+				'content-type': 'text/html',
+			}
+		})
 	},
 };
